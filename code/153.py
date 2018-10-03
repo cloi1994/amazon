f98f4dcbe4e550d0ab6fbcefcd3d22a9b2eb6d3d
cloi1994 <31608472+cloi1994@users.noreply.github.com>
2018-10-02 19:07:49 -0700

    Create 153.py

---
+++ code/153.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def findMin(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        
+        i = 0
+        j = len(nums) - 1
+        
+            
+        while i < j:
+            
+            mid = i + (j - i) // 2
+            
+            if nums[mid] > nums[j]:
+                i = mid + 1
+            else:
+                j = mid
+                
+                
+        return nums[i]
+            
+            
+        
